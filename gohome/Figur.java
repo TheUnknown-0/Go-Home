665be8335f3cf67a23d8fee098721494f88d6ea1
TheUnknown-0 <minecraft-mo@posteo.de>
2026-01-15 14:09:35 +0100

    Add files via upload

--- gohome/Figur.java
+++ gohome/Figur.java
@@ -1,9 +1,9 @@
 package gohome;
 
 public class Figur {
-    public String farbe;
-    public int x;
-    public int y;
+    public String farbe; // Farbe, um Farbe des Spielers zu bestimmen
+    public int x;   // Position auf der X achse
+    public int y;   // -||- Y -||-
 
     public Figur(String farbe, int startX, int startY) {
         this.farbe = farbe;
@@ -12,16 +12,16 @@ public class Figur {
     }
 
     public boolean gewonnen() {
-        return x == 2 && y == 2;
+        return x == 2 && y == 2; // Gewinnbedingung
     }
 
     // richtung: 0=oben,1=rechts,2=unten,3=links
     public void gehe(int richtung) {
         switch (richtung) {
-            case 0: y = (y - 1 + 5) % 5; break;
-            case 1: x = (x + 1) % 5; break;
-            case 2: y = (y + 1) % 5; break;
-            case 3: x = (x - 1 + 5) % 5; break;
+            case 0: y = (y - 1 + 5) % 5; break; // mit dem Modulo verhindern, dass jemand außerhalb des Spiels ist
+            case 1: x = (x + 1) % 5; break; // mit dem Modulo verhindern, dass jemand außerhalb des Spiels ist
+            case 2: y = (y + 1) % 5; break; // mit dem Modulo verhindern, dass jemand außerhalb des Spiels ist
+            case 3: x = (x - 1 + 5) % 5; break; // mit dem Modulo verhindern, dass jemand außerhalb des Spiels ist
         }
     }
 }
